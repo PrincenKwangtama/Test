52ae307f2bb65e73be3f225891c2072039e6adc2
KevinLenon <95207790+KevinLenon@users.noreply.github.com>
2022-04-11 09:13:57 +0800

    Update Assets_3D.cs

--- Assets_3D.cs
+++ Assets_3D.cs
@@ -1,4 +1,4 @@
-﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Desktop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -84,7 +84,6 @@ namespace Test
             GL.BindVertexArray(_vertexArrayObject);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
-
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
             GL.EnableVertexAttribArray(1);*/
 
@@ -109,7 +108,7 @@ namespace Test
             }
         }
 
-        public void render(float r, float g, float b, int pilihan,double time, Matrix4 temp)
+        public void render(float r, float g, float b, int pilihan, double time, Matrix4 temp)
         {
             _shader.Use();
 
@@ -137,7 +136,6 @@ namespace Test
             if (_indices.Count != 0)
             {
                 GL.DrawElements(PrimitiveType.Triangles, _indices.Count, DrawElementsType.UnsignedInt, 0);
-                Console.WriteLine(time);
             }
             else
             {
@@ -161,7 +159,7 @@ namespace Test
             }
             foreach (var item in Child)
             {
-                item.render(r,g,b,pilihan, time, temp);
+                item.render(r, g, b, pilihan, time, temp);
             }
 
 
@@ -243,6 +241,235 @@ namespace Test
                 3,6,7
             };
         }
+        public void createReversePyramidBox(float x, float y, float z, float length)
+        {
+            _centerPosition.X = x;
+            _centerPosition.Y = y;
+            _centerPosition.Z = z;
+            Vector3 temp_vector;
+
+            //TITIK 1
+            temp_vector.X = x - length / 2.0f;
+            temp_vector.Y = y + length / 2.0f;
+            temp_vector.Z = z - length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 2
+            temp_vector.X = x + length / 2.0f;
+            temp_vector.Y = y + length / 2.0f;
+            temp_vector.Z = z - length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 3
+            temp_vector.X = x - length / 4.0f;
+            temp_vector.Y = y - length / 4.0f;
+            temp_vector.Z = z - length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 4
+            temp_vector.X = x + length / 4.0f;
+            temp_vector.Y = y - length / 4.0f;
+            temp_vector.Z = z - length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 5
+            temp_vector.X = x - length / 2.0f;
+            temp_vector.Y = y + length / 2.0f;
+            temp_vector.Z = z + length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 6
+            temp_vector.X = x + length / 2.0f;
+            temp_vector.Y = y + length / 2.0f;
+            temp_vector.Z = z + length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 7
+            temp_vector.X = x - length / 4.0f;
+            temp_vector.Y = y - length / 4.0f;
+            temp_vector.Z = z + length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 8
+            temp_vector.X = x + length / 4.0f;
+            temp_vector.Y = y - length / 4.0f;
+            temp_vector.Z = z + length / 4.0f;
+            _vertices.Add(temp_vector);
+
+            _indices = new List<uint>
+            {
+                //SEGITIGA DEPAN 1
+                0,1,2,
+                //SEGITIGA DEPAN 2
+                1,2,3,
+                //SEGITIGA ATAS 1
+                0,4,5,
+                //SEGITIGA ATAS 2
+                0,1,5,
+                //SEGITIGA KANAN 1
+                1,3,5,
+                //SEGITIGA KANAN 2
+                3,5,7,
+                //SEGITIGA KIRI 1
+                0,2,4,
+                //SEGITIGA KIRI 2
+                2,4,6,
+                //SEGITIGA BELAKANG 1
+                4,5,6,
+                //SEGITIGA BELAKANG 2
+                5,6,7,
+                //SEGITIGA BAWAH 1
+                2,3,6,
+                //SEGITIGA BAWAH 2
+                3,6,7
+            };
+        }
+        public void createPyramidBox(float x, float y, float z, float length)
+        {
+            _centerPosition.X = x;
+            _centerPosition.Y = y;
+            _centerPosition.Z = z;
+            Vector3 temp_vector;
+
+            //TITIK 1
+            temp_vector.X = x - length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z - length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 2
+            temp_vector.X = x + length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z - length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 3
+            temp_vector.X = x - length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z - length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 4
+            temp_vector.X = x + length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z - length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 5
+            temp_vector.X = x - length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z + length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 6
+            temp_vector.X = x + length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z + length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 7
+            temp_vector.X = x - length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z + length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 8
+            temp_vector.X = x + length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z + length / 2.0f;
+            _vertices.Add(temp_vector);
+
+            _indices = new List<uint>
+            {
+                //SEGITIGA DEPAN 1
+                0,1,2,
+                //SEGITIGA DEPAN 2
+                1,2,3,
+                //SEGITIGA ATAS 1
+                0,4,5,
+                //SEGITIGA ATAS 2
+                0,1,5,
+                //SEGITIGA KANAN 1
+                1,3,5,
+                //SEGITIGA KANAN 2
+                3,5,7,
+                //SEGITIGA KIRI 1
+                0,2,4,
+                //SEGITIGA KIRI 2
+                2,4,6,
+                //SEGITIGA BELAKANG 1
+                4,5,6,
+                //SEGITIGA BELAKANG 2
+                5,6,7,
+                //SEGITIGA BAWAH 1
+                2,3,6,
+                //SEGITIGA BAWAH 2
+                3,6,7
+            };
+        }
+        public void createJajarBox(float x, float y, float z, float length)
+        {
+            _centerPosition.X = x;
+            _centerPosition.Y = y;
+            _centerPosition.Z = z;
+            Vector3 temp_vector;
+
+            //TITIK 1
+            temp_vector.X = x - length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z - length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 2
+            temp_vector.X = x + length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z - length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 3
+            temp_vector.X = x - length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z - length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 4
+            temp_vector.X = x + length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z - length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 5
+            temp_vector.X = x - length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z + length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 6
+            temp_vector.X = x + length / 4.0f;
+            temp_vector.Y = y + length / 4.0f;
+            temp_vector.Z = z + length / 4.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 7
+            temp_vector.X = x - length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z + length / 2.0f;
+            _vertices.Add(temp_vector);
+            //TITIK 8
+            temp_vector.X = x + length / 2.0f;
+            temp_vector.Y = y - length / 2.0f;
+            temp_vector.Z = z + length / 2.0f;
+            _vertices.Add(temp_vector);
+
+            _indices = new List<uint>
+            {
+                //SEGITIGA DEPAN 1
+                0,1,2,
+                //SEGITIGA DEPAN 2
+                1,2,3,
+                //SEGITIGA ATAS 1
+                0,4,5,
+                //SEGITIGA ATAS 2
+                0,1,5,
+                //SEGITIGA KANAN 1
+                1,3,5,
+                //SEGITIGA KANAN 2
+                3,5,7,
+                //SEGITIGA KIRI 1
+                0,2,4,
+                //SEGITIGA KIRI 2
+                2,4,6,
+                //SEGITIGA BELAKANG 1
+                4,5,6,
+                //SEGITIGA BELAKANG 2
+                5,6,7,
+                //SEGITIGA BAWAH 1
+                2,3,6,
+                //SEGITIGA BAWAH 2
+                3,6,7
+            };
+        }
+
         public void createEllipsoid(float radiusX, float radiusY, float radiusZ, float _x, float _y, float _z)
         {
             _centerPosition.X = _x;
@@ -258,6 +485,57 @@ namespace Test
                     temp_vector.Y = _y + (float)Math.Cos(v) * (float)Math.Sin(u) * radiusY;
                     temp_vector.Z = _z + (float)Math.Sin(v) * radiusZ;
                     _vertices.Add(temp_vector);
+                }
+            }
+        }
+        public void createNewEllipsoid(float radiusX, float radiusY, float radiusZ, float _x, float _y, float _z, int sectorCount, int stackCount)
+        {
+            _centerPosition.X = _x;
+            _centerPosition.Y = _y;
+            _centerPosition.Z = _z;
+            float pi = (float)Math.PI;
+            Vector3 temp_vector;
+            float sectorStep = 2 * (float)Math.PI / sectorCount;
+            float stackStep = (float)Math.PI / stackCount;
+            float sectorAngle, StackAngle, x, y, z;
+
+            for (int i = 0; i <= stackCount; ++i)
+            {
+                StackAngle = pi / 2 - i * stackStep;
+                x = radiusX * (float)Math.Cos(StackAngle);
+                y = radiusY * (float)Math.Cos(StackAngle);
+                z = radiusZ * (float)Math.Sin(StackAngle);
+
+                for (int j = 0; j <= sectorCount; ++j)
+                {
+                    sectorAngle = j * sectorStep;
+
+                    temp_vector.X = x * (float)Math.Cos(sectorAngle);
+                    temp_vector.Y = y * (float)Math.Sin(sectorAngle);
+                    temp_vector.Z = z;
+                    _vertices.Add(temp_vector);
+                }
+            }
+
+            uint k1, k2;
+            for (int i = 0; i < stackCount; ++i)
+            {
+                k1 = (uint)(i * (sectorCount + 1));
+                k2 = (uint)(k1 + sectorCount + 1);
+                for (int j = 0; j < sectorCount; ++j, ++k1, ++k2)
+                {
+                    if (i != 0)
+                    {
+                        _indices.Add(k1);
+                        _indices.Add(k2);
+                        _indices.Add(k1 + 1);
+                    }
+                    if (i != (stackCount - 1))
+                    {
+                        _indices.Add(k1 + 1);
+                        _indices.Add(k2);
+                        _indices.Add(k2 + 1);
+                    }
                 }
             }
         }
@@ -309,6 +587,57 @@ namespace Test
                         _indices.Add(k2);
                         _indices.Add(k2 + 1);
                     }
+                }
+            }
+        }
+
+        public void createTorus(float x, float y, float z, float radMajor, float radMinor, float sectorCount, float stackCount)
+        {
+            _centerPosition.X = x;
+            _centerPosition.Y = y;
+            _centerPosition.Z = z;
+
+            float pi = (float)Math.PI;
+            Vector3 temp_vector;
+            stackCount *= 2;
+            float sectorStep = 2 * pi / sectorCount;
+            float stackStep = 2 * pi / stackCount;
+            float sectorAngle, stackAngle, tempX, tempY, tempZ;
+
+            for (int i = 0; i <= stackCount; ++i)
+            {
+                stackAngle = pi / 2 - i * stackStep;
+                tempX = radMajor + radMinor * (float)Math.Cos(stackAngle);
+                tempY = radMinor * (float)Math.Sin(stackAngle);
+                tempZ = radMajor + radMinor * (float)Math.Cos(stackAngle);
+
+                for (int j = 0; j <= sectorCount; ++j)
+                {
+                    sectorAngle = j * sectorStep;
+
+                    temp_vector.X = x + tempX * (float)Math.Cos(sectorAngle);
+                    temp_vector.Y = y + tempY;
+                    temp_vector.Z = z + tempZ * (float)Math.Sin(sectorAngle);
+
+                    _vertices.Add(temp_vector);
+                }
+            }
+
+            uint k1, k2;
+            for (int i = 0; i < stackCount; ++i)
+            {
+                k1 = (uint)(i * (sectorCount + 1));
+                k2 = (uint)(k1 + sectorCount + 1);
+
+                for (int j = 0; j < sectorCount; ++j, ++k1, ++k2)
+                {
+                    _indices.Add(k1);
+                    _indices.Add(k2);
+                    _indices.Add(k1 + 1);
+
+                    _indices.Add(k1 + 1);
+                    _indices.Add(k2);
+                    _indices.Add(k2 + 1);
                 }
             }
         }
@@ -412,10 +741,22 @@ namespace Test
             }
         }
 
-        public void addChild(float x, float y, float z, float length)
+        public void addChild(float x, float y, float z, float length,int pilihan)
         {
             Assets_3D newChild = new Assets_3D();
-            newChild.createBoxVertices(x, y, z, length);
+            if(pilihan == 0)
+            {
+                newChild.createBoxVertices(x, y, z, length);
+            }
+            else if(pilihan == 1)
+            {
+                newChild.createPyramidBox(x, y, z, length);
+            }
+            else if (pilihan == 2)
+            {
+                newChild.createJajarBox(x, y, z, length);            
+            }
+            
             Child.Add(newChild);
         }
     }
